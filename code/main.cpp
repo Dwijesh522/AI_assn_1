041def508754c3bbeb57968aa6dfe28c11794078
dwijesh522 <iddwijesh@gmail.com>
2019-08-21 13:14:25 +0530

    it works

--- code/main.cpp
+++ code/main.cpp
@@ -6,42 +6,44 @@ using namespace std;
 
 int main()
 {
-	vector<int> gene1, gene2, gene3;
-	// -ACTGTGA
-	(gene1).push_back(4);
-	(gene1).push_back(0);
-	(gene1).push_back(1);
-	(gene1).push_back(2);
-	(gene1).push_back(3);
-	(gene1).push_back(2);
-	(gene1).push_back(3);
-	(gene1).push_back(0);
-	// TACT--GC
-	(gene2).push_back(2);
-	(gene2).push_back(0);
-	(gene2).push_back(1);
-	(gene2).push_back(2);
-	(gene2).push_back(4);
-	(gene2).push_back(4);
-	(gene2).push_back(3);
-	(gene2).push_back(1);
-	// -ACTG--A
-	(gene3).push_back(4);
-	(gene3).push_back(0);
-	(gene3).push_back(1);
-	(gene3).push_back(2);
-	(gene3).push_back(3);
-	(gene3).push_back(4);
-	(gene3).push_back(4);
-	(gene3).push_back(0);
-	
 	state state1 = state();
-	state1.push_gene_seq(&gene1);
-	cout << state1.get_cost() << endl;
-	state1.push_gene_seq(&gene2);
-	cout << state1.get_cost() << endl;
-	state1.push_gene_seq(&gene3);
-
+	for(int i=0; i<1; i++)
+	{
+		vector<int> gene1, gene2, gene3;
+		// -ACTGTGA
+		(gene1).push_back(4);
+		(gene1).push_back(0);
+		(gene1).push_back(1);
+		(gene1).push_back(2);
+		(gene1).push_back(3);
+		(gene1).push_back(2);
+		(gene1).push_back(3);
+		(gene1).push_back(0);
+		// TACT--GC
+		(gene2).push_back(2);
+		(gene2).push_back(0);
+		(gene2).push_back(1);
+		(gene2).push_back(2);
+		(gene2).push_back(4);
+		(gene2).push_back(4);
+		(gene2).push_back(3);
+		(gene2).push_back(1);
+		// -ACTG--A
+		(gene3).push_back(4);
+		(gene3).push_back(0);
+		(gene3).push_back(1);
+		(gene3).push_back(2);
+		(gene3).push_back(3);
+		(gene3).push_back(4);
+		(gene3).push_back(4);
+		(gene3).push_back(0);
+	
+		state1.push_gene_seq(&gene1);
+		cout << state1.get_cost() << endl;
+		state1.push_gene_seq(&gene2);
+		cout << state1.get_cost() << endl;
+		state1.push_gene_seq(&gene3);
+	}
 	cout << state1.get_cost() << endl;
 	return 0;
 }
